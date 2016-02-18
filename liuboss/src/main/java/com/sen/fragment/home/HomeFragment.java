bae792bbad8d36ab961bed10b4a30dcec9554e3e
TangGSen <Jasonsens17@gmail.com>
2016-02-18 16:09:10 +0800

    viewPager

--- liuboss/src/main/java/com/sen/fragment/home/HomeFragment.java
+++ liuboss/src/main/java/com/sen/fragment/home/HomeFragment.java
@@ -1,13 +1,21 @@
 package com.sen.fragment.home;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 import com.sen.base.BaseFragment;
 import com.sen.liuboss.R;
+import com.sen.widget.ImageCycleView;
 
+import java.util.ArrayList;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -15,23 +23,74 @@ import butterknife.ButterKnife;
  */
 public class HomeFragment extends BaseFragment {
     private View rootView;
+    private ArrayList<String> urls = new ArrayList<>();
+    @Bind(R.id.head_viewpager)
+    ImageCycleView head_viewpager;
+
+    @Bind(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected View initViews(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_home_act_home, container, false);
         ButterKnife.bind(this, rootView);
+        //处理适配 设置比例
+        dealAdaptationToPhone();
+
 
 
 
         return rootView;
     }
+
+    private void dealAdaptationToPhone() {
+
+        WindowManager windowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+
+        ViewGroup.LayoutParams params = head_viewpager.getLayoutParams();
+        params.height = (int) (dm.widthPixels * 0.421875);
+        head_viewpager.setLayoutParams(params);
+
+        ViewGroup.LayoutParams collapsingLayoutParams = collapsingToolbarLayout.getLayoutParams();
+        collapsingLayoutParams.height = (int) (dm.widthPixels);
+        collapsingToolbarLayout.setLayoutParams(collapsingLayoutParams);
+
+
+    }
+
     @Override
     protected void initData() {
+        //注意，懒加载 一定要调用 setUserVisibleHint(true)后该方法才调用
+        urls.add("http://s.xnimg.cn/rrinvest/wap/events/2015/recruitment/img/banner.jpg");
+        urls.add("http://a.xnimg.cn/rrinvest/img/bannerwap/2015-11-11.jpg");
+        urls.add("http://s.xnimg.cn/rrinvest/wap/events/2015/fall/img/appbanner.png");
+        urls.add("http://fmn.rrfmn.com/fmn074/20150413/1720/original_WyLG_60c800005a561e83.jpg");
+        head_viewpager.setImageResources(urls, new ImageCycleView.ImageCycleViewListener() {
+            @Override
+            public void onImageClick(int position, View imageView) {
 
+            }
+        }, 0);
     }
 
     @Override
     protected void setDefaultFragmentTitle(String title) {
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        head_viewpager.startImageCycle();
+        setUserVisibleHint(true);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        head_viewpager.stopImageCycle();
+        setUserVisibleHint(false);
     }
 }
--- liuboss/src/main/java/com/sen/uitls/SenApplication.java
+++ liuboss/src/main/java/com/sen/uitls/SenApplication.java
@@ -1,6 +1,17 @@
 package com.sen.uitls;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.Environment;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+
+import java.io.File;
 
 /**
  * Created by Sen on 2016/1/6.
@@ -11,10 +22,40 @@ public class SenApplication extends Application {
         super.onCreate();
 
         initBmob();
+        initImageLoader(this);//初始化图像缓存加载器
     }
 
     private void initBmob() {
 
         //sBmob.initialize(this,"6d66f030aaf8a34c92f5b13d13b3d0dc");
+    }
+
+    /**
+     * 初始化ImageLoader
+     */
+    @SuppressWarnings("deprecation")
+    public static void initImageLoader(Context context) {
+        File cacheDir = new File(Environment.getExternalStorageDirectory()
+                + "/cache");// 获取到缓存的目录地址
+        // 创建配置ImageLoader(所有的选项都是可选的,只使用那些你真的想定制)，这个可以设定在APPLACATION里面，设置为全局的配置参数
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                context)
+                // 线程池内加载的数量
+                .writeDebugLogs()
+                .threadPoolSize(4).threadPriority(Thread.NORM_PRIORITY - 2)
+                .memoryCache(new WeakMemoryCache())
+                .denyCacheImageMultipleSizesInMemory()
+                .discCacheFileNameGenerator(new Md5FileNameGenerator())
+                // 将保存的时候的URI名称用MD5 加密
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .diskCacheSize(50 * 1024 * 1024)
+                .discCache(new UnlimitedDiskCache(cacheDir))// 自定义缓存路径
+                // .defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+                .writeDebugLogs() // Remove for release app
+                .build();
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config);// 全局初始化此配置
+
+        com.nostra13.universalimageloader.utils.L.writeLogs(false);
     }
 }
