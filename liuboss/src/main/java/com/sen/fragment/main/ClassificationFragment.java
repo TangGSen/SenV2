7d45be45bab60e4706f8ce2c43dfbb71a52b8d1e
TangGSen <Jasonsens17@gmail.com>
2016-02-24 15:12:47 +0800

    class tab

--- liuboss/src/main/java/com/sen/fragment/main/ClassificationFragment.java
+++ liuboss/src/main/java/com/sen/fragment/main/ClassificationFragment.java
@@ -1,14 +1,21 @@
 package com.sen.fragment.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.view.WindowManager;
 
+import com.sen.adapter.HomeActFragAdpter;
 import com.sen.base.BaseFragment;
 import com.sen.liuboss.R;
+import com.sen.uitls.ResourcesUtils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -20,8 +27,15 @@ public class ClassificationFragment extends BaseFragment {
     private View rootView;
     @Bind(R.id.class_tablayout)
     TabLayout class_tablayout;
-    @Bind(R.id.class_head_layout)
-    LinearLayout class_head_layout;
+    @Bind(R.id.class_toolbar)
+    Toolbar class_toolbar;
+    @Bind(R.id.tv_search)
+    AppCompatTextView tv_search;
+    @Bind(R.id.class_viewpager_shows)
+    ViewPager class_viewpager_shows;
+
+    private String tabTiles[];
+    private int tabCount ;
 
 
     @Override
@@ -29,13 +43,73 @@ public class ClassificationFragment extends BaseFragment {
         rootView = inflater.inflate(R.layout.fragment_main_act_classification, container, false);
         ButterKnife.bind(this, rootView);
         initToolbarAndTab(rootView);
+        dealAdaptationToPhone();
+        initTabView();
         return rootView;
     }
 
-    private void initToolbarAndTab(View rootView) {
-      View toolbarView = View.inflate(getContext(), R.layout.class_toolbar_layout, null);
+    private void initTabView() {
+        //init tab data
+        tabTiles = ResourcesUtils.getStringArray(getContext(), R.array.taHomeItemName);
+        final HomeActFragAdpter fragAdapter = new HomeActFragAdpter(getActivity().getSupportFragmentManager(), getContext(), tabTiles);
+        class_viewpager_shows.setAdapter(fragAdapter);
+        class_tablayout.setupWithViewPager(class_viewpager_shows);
 
-        class_head_layout.addView(toolbarView);
+        tabCount = class_tablayout.getTabCount();
+        for (int i = 0; i < tabCount; i++) {
+            TabLayout.Tab tab = class_tablayout.getTabAt(i);
+            if (tab != null) {
+                tab.setCustomView(getTabView(tabTiles[i]));
+
+            }
+        }
+//        home_tablayout.getTabAt(0).select(); 无效
+        AppCompatTextView compatTextView = (AppCompatTextView) class_tablayout.getTabAt(0).getCustomView();
+        compatTextView.setSelected(true);
+        compatTextView.setBackgroundDrawable(ResourcesUtils.getResDrawable(getContext(), R.drawable.home_tab_background));
+        class_viewpager_shows.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //重设textview 的背景
+                for (int i = 0; i < tabCount; i++) {
+                    AppCompatTextView compatTextView = (AppCompatTextView) class_tablayout.getTabAt(i).getCustomView();
+                    if (i == position) {
+                        compatTextView.setBackgroundDrawable(ResourcesUtils.getResDrawable(getContext(), R.drawable.home_tab_background));
+                    } else {
+                        compatTextView.setBackgroundDrawable(null);
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+
+    private void dealAdaptationToPhone() {
+        WindowManager windowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(dm);
+
+        ViewGroup.LayoutParams params = tv_search.getLayoutParams();
+        params.width = (int) (dm.widthPixels * 0.618);
+        tv_search.setLayoutParams(params);
+    }
+
+    private void initToolbarAndTab(View rootView) {
+
     }
 
     @Override
@@ -46,5 +120,14 @@ public class ClassificationFragment extends BaseFragment {
     @Override
     protected void setDefaultFragmentTitle(String title) {
 
+    }
+
+    //自定义TabView
+    public View getTabView(String text) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.home_inner_item_tab, null);
+        AppCompatTextView textView = (AppCompatTextView) view.findViewById(R.id.inner_tab_name);
+        textView.setText(text);
+        textView.setTextSize(16);
+        return view;
     }
 }
