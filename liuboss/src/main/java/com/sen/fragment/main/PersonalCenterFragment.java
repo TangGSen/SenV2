10ea1c6a9e35a58637ce6e90d82e9721009542b5
TangGSen <Jasonsens17@gmail.com>
2016-03-05 21:02:33 +0800

    personal img setting

--- liuboss/src/main/java/com/sen/fragment/main/PersonalCenterFragment.java
+++ liuboss/src/main/java/com/sen/fragment/main/PersonalCenterFragment.java
@@ -2,10 +2,13 @@ package com.sen.fragment.main;
 
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.sen.base.BaseFragment;
 import com.sen.liuboss.R;
@@ -19,15 +22,30 @@ import butterknife.ButterKnife;
  */
 public class PersonalCenterFragment extends BaseFragment {
     private View rootView;
-        @Bind(R.id.personal_rootLayout)
-        CoordinatorLayout personal_rootLayout ;
+    @Bind(R.id.personal_rootLayout)
+    CoordinatorLayout personal_rootLayout;
+    @Bind(R.id.img_user)
+    AppCompatImageView img_user;
 
     @Override
     protected void dealAdaptationToPhone() {
         //动态设置coordinatorLayout 的marginTop,因为在小米的手机xml 的24dp中显示不正常
         FrameLayout.LayoutParams rootLayoutParams = (FrameLayout.LayoutParams) personal_rootLayout.getLayoutParams();
-        rootLayoutParams.setMargins(0, ResourcesUtils.getStatusBarHeight(getContext()),0,0);
+        rootLayoutParams.setMargins(0, ResourcesUtils.getStatusBarHeight(getContext()), 0, 0);
         personal_rootLayout.setLayoutParams(rootLayoutParams);
+
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, (int) (getTabLayoutHeight()*1.5), 0, 0);
+        img_user.setLayoutParams(lp);
+    }
+
+    private int getTabLayoutHeight() {
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (getContext().getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, getContext().getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
     }
 
 
@@ -37,6 +55,7 @@ public class PersonalCenterFragment extends BaseFragment {
         ButterKnife.bind(this, rootView);
         return rootView;
     }
+
     @Override
     protected void initData() {
 
